6f5a404365a4974ff2a6c4fd90faa7987d7df727
luba150278 <60192622+luba150278@users.noreply.github.com>
2020-01-27 17:51:23 +0200

    Add files via upload
    js slider

--- dz_150120/assets/js/code_js.js
+++ dz_150120/assets/js/code_js.js
@@ -15,6 +15,14 @@ slid.onclick = function (event) {
 	if(name_a=='a4'){txt=mas[3];im=mas_img[3];}
 	if(name_a=='a5'){txt=mas[4];im=mas_img[4];}
 	 document.getElementById('slogan').innerHTML=txt;
+	 var bgs='assets/img/slider_select.png';
+ 	var bgs1='assets/img/slider.png';
+//document.event.target.id.style.background = 'url("'+bgs+'")';
+	
+  	document.getElementById('top_main').style.background ='url("'+im+'")';
 
-  document.getElementById('top_main').style.background ='url("'+im+'")';
+  	for (var i = 0; i < this.children.length; i++) {  		
+     if(this.children[i].id==name_a){this.children[i].style.background= 'url("'+bgs+'") no-repeat center top ';}
+     else{this.children[i].style.background= 'url("'+bgs1+'") no-repeat center top ';}
+    }
 }
